61744e3c94f32767191881856f4bcfdedac45f1b
Rahul Sridhar <rahulsridhar1996@gmail.com>
2020-02-11 23:44:39 +0100

    updated the radix_exec.sh file

--- lab1/avdark-cache/radix_exec.sh
+++ lab1/avdark-cache/radix_exec.sh
@@ -1,12 +1,12 @@
 #Executes the radix codes
 
-./pin-avdc.sh -s 16384 -l 32 -a 1 -- ../radix/radix -n 100000 -o radix1.out
-./pin-avdc.sh -s 16384 -l 32 -a 2 -- ../radix/radix -n 100000 -o radix2.out
-./pin-avdc.sh -s 32768 -l 16 -a 1 -- ../radix/radix -n 100000 -o radix3.out
-./pin-avdc.sh -s 32768 -l 32 -a 1 -- ../radix/radix -n 100000 -o radix4.out
-./pin-avdc.sh -s 32768 -l 64 -a 1 -- ../radix/radix -n 100000 -o radix5.out
-./pin-avdc.sh -s 32768 -l 16 -a 2 -- ../radix/radix -n 100000 -o radix6.out
-./pin-avdc.sh -s 32768 -l 32 -a 2 -- ../radix/radix -n 100000 -o radix7.out
-./pin-avdc.sh -s 32768 -l 64 -a 2 -- ../radix/radix -n 100000 -o radix8.outv
-./pin-avdc.sh -s 65536 -l 32 -a 1 -- ../radix/radix -n 100000 -o radix9.out
-./pin-avdc.sh -s 65536 -l 32 -a 2 -- ../radix/radix -n 100000 -o radix10.out
+./pin-avdc.sh -s 16384 -l 32 -a 1 -o radix1.out -- ../radix/radix -n 100000 
+./pin-avdc.sh -s 16384 -l 32 -a 2 -o radix2.out -- ../radix/radix -n 100000 
+./pin-avdc.sh -s 32768 -l 16 -a 1 -o radix3.out -- ../radix/radix -n 100000 
+./pin-avdc.sh -s 32768 -l 32 -a 1 -o radix4.out -- ../radix/radix -n 100000 
+./pin-avdc.sh -s 32768 -l 64 -a 1 -o radix5.out -- ../radix/radix -n 100000 
+./pin-avdc.sh -s 32768 -l 16 -a 2 -o radix6.out -- ../radix/radix -n 100000 
+./pin-avdc.sh -s 32768 -l 32 -a 2 -o radix7.out -- ../radix/radix -n 100000 
+./pin-avdc.sh -s 32768 -l 64 -a 2 -o radix8.out -- ../radix/radix -n 100000
+./pin-avdc.sh -s 65536 -l 32 -a 1 -o radix9.out -- ../radix/radix -n 100000 
+./pin-avdc.sh -s 65536 -l 32 -a 2 -o radix10.out -- ../radix/radix -n 100000 
